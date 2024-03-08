4096898e3837c250d2282c53bf7edb8e9bd8a720
Jorge Alvarez <alvarezmolina2398@gmail.com>
2024-03-08 12:02:48 -0600

    add version qr view

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var express = require("express");
-var app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
-//const fetch = require('node-fetch');
-var messages = [];
+
+import express from "express";
+const app = express();
+import http from "http";
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+import fetch from 'node-fetch';
+
+const messages = [];
 
 app.use(express.static("public"));
 
-
-app.get("/hello", function (req, res) {
+app.get("/hello", (req, res) => {
   res.status(200).send("Hello World!");
 });
 
-
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
   console.log("Alguien se ha conectado con Sockets");
 
-  // Manejar el evento de unirse a una sala
-  socket.on("join-room", function (username) {
+  socket.on("join-room", (username) => {
     socket.join(username);
     console.log(`${username} se ha unido a la sala ${username}`);
   });
 
-  // Emitir los mensajes a la sala específica del usuario
-  socket.on("new-message", function (data) {
+  socket.on("new-message", (data) => {
     try {
       const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
@@ -32,7 +32,7 @@ io.on("connection", function (socket) {
       const raw = JSON.stringify({
         "sesion": data.sesion ?? "",
         "comercio": data.comercio ?? "",
-        "monto": data.monto ?? 0,
+        "monto": data.monto  ?? 0,
         "usuario_id": data.usuario ?? 'SIN USUARIO',
         "accion": data.accion ?? 0
       });
@@ -44,37 +44,24 @@ io.on("connection", function (socket) {
         redirect: "follow"
       };
 
-      import('node-fetch').then((fetch) => {
-        fetch("https://appsip.genesisempresarial.com/GEfectivoAdmin/api/LogBotonPago/InsertLogBotonPago", requestOptions)
-          .then((response) => response.text())
-          .then((result) => console.log(result))
-          .catch((error) => console.error(error));
+      fetch("https://appsip.genesisempresarial.com/GEfectivoAdmin/api/LogBotonPago/InsertLogBotonPago", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
 
-      }).catch(error => console.error('Error al importar node-fetch:', error));;
     } catch (e) {
       console.log(e);
     }
 
-
-    //console.log(data);
     messages.push(data);
-    const messagesReturn = messages.filter(m => m.sesion == data.sesion);
-    const validos = messagesReturn.filter(m => m.accion == 1);
+    const messagesReturn = messages.filter((m) => m.sesion == data.sesion);
+    const validos = messagesReturn.filter((m) => m.accion == 1);
 
     console.log(validos.length);
-    // console.log(messages);
-    // console.log(messagesReturn);
-    // Enviar mensajes solo a la sala del usuario que los envió
     io.to(data.sesion).emit("messages", validos.length != 0 ? messagesReturn : []);
   });
-
-
 });
 
-
-
-server.listen(8080, function () {
+server.listen(8080, () => {
   console.log("Servidor corriendo en http://localhost:8080");
 });
-
-
