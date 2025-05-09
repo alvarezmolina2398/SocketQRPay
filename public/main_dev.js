8049a35e1876ae7a790ff0d57724ce02b2b020c6
Walfred Donis <walfreddonis1@gmail.com>
2025-05-09 09:31:05 -0600

    Implementacion de nuevo parametro para POS - billreference

--- public/main_dev.js
+++ public/main_dev.js
@@ -25,6 +25,8 @@ $(function () {
     const monto = urlParams.get('monto');
     const usuario = urlParams.get('usuario');
     const sesion = urlParams.get('sesion');
+    const bill_reference = urlParams.get('bill_reference') || null;
+
     montoTxt.text('Q' + parseFloat(monto).toFixed(2));
 
     const sesionQR = generarCadenaAleatoria(6);
@@ -40,7 +42,7 @@ $(function () {
 
         if (diferenciaTiempo <= 0) {
             clearInterval(intervalo);
-            socket.emit("new-message", { sesion, comercio, monto, accion: 5 , usuario, sesionQR, env: "dev"});
+            socket.emit("new-message", { sesion, comercio, monto, accion: 5 , usuario, sesionQR, env: "dev", bill_reference });
         } else {
             const minutosRestantes = Math.floor((diferenciaTiempo / 1000 / 60) % 60);
             const segundosRestantes = Math.floor((diferenciaTiempo / 1000) % 60);
@@ -49,7 +51,7 @@ $(function () {
     }, 1000);
 
     socket.emit("join-room", sesion);
-    socket.emit("new-message", { sesion, comercio, monto, accion: 1, usuario, sesionQR, env: "dev"});
+    socket.emit("new-message", { sesion, comercio, monto, accion: 1, usuario, sesionQR, env: "dev", bill_reference});
 
     socket.on("messages", function (data) {
         const message = data[data.length - 1];
